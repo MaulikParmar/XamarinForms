4ce25cc50fe6778e7766222d84b3b1348dd98c80
MAULIK <MAULIK@MAULIK-PC>
2016-12-24 20:59:59 +0530

    Removed namespace from validation base

--- ValidationDemo/ValidationDemo/ValidationDemo/ValidationBase.cs
+++ ValidationDemo/ValidationDemo/ValidationDemo/ValidationBase.cs
@@ -5,8 +5,6 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ValidationDemo
 {
