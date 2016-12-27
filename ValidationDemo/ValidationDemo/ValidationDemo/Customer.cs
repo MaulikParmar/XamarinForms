af328a38dee7f163a247e0a8fb974624b5d38ee6
MAULIK <MAULIK@MAULIK-PC>
2016-12-27 21:38:20 +0530

    changes

--- ValidationDemo/ValidationDemo/ValidationDemo/Customer.cs
+++ ValidationDemo/ValidationDemo/ValidationDemo/Customer.cs
@@ -5,7 +5,7 @@ namespace ValidationDemo
     public class Customer : ModelBase
     {
         private string _firstName;
-        [Display(Name = "First Name")]
+        [Display(Name = "First name")]
         [Required]
         [StringLength(20)]
         public string FirstName
